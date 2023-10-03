ff83b6970a1461c447dc214761d7bc6978d48aee
thatgreenmario <oj1696@gmail.com>
2023-10-03 20:50:43 +0530

    added skeleton code for lists

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainboardComponent } from './mainboard/mainboard.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mainboard/mainboard.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop'
+@Component({
+  selector: 'app-mainboard',
+  templateUrl: './mainboard.component.html',
+  styleUrls: ['./mainboard.component.css'],
+})
+export class MainboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  Schedule = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  Do = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  Delete = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  Delegate = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  ScratchPad = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+}
